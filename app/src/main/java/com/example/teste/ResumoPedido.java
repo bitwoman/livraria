56e5a08c16ce5b2d935b4eb1484702a239ba764e
Brenda <brenda.leite1@hotmail.com>
2021-12-18 13:40:25 -0300

    teste

---
+++ app/src/main/java/com/example/teste/ResumoPedido.java
@@ -0,0 +1,23 @@
+package com.example.teste;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ResumoPedido extends Fragment {
+
+    public ResumoPedido() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_resumo_pedido, container, false);
+    }
+    
+    
+}
--- app/src/main/java/com/example/teste/ui/gallery/GerenciandoLivrosViewModel.java
+++ app/src/main/java/com/example/teste/ui/gallery/GerenciandoLivrosViewModel.java
@@ -10,7 +10,7 @@ public class GerenciandoLivrosViewModel extends ViewModel {
 
     public GerenciandoLivrosViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        //mText.setValue("This is gallery fragment");
     }
 
     public LiveData<String> getText() {
